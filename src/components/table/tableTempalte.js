da2e714bafb9129701bb2896ee27cd265bc7bacf
lensroman <68300112+lensroman@users.noreply.github.com>
2021-04-07 20:17:33 +0300

    Finish table resize

--- src/components/table/tableTempalte.js
+++ src/components/table/tableTempalte.js
@@ -2,24 +2,29 @@ const CODES = {
     A: 65,
     Z: 90,
 }
-
-function createCell() {
+function createCell(_, colIndex) {
     return `
-        <div class="cell" contenteditable=""></div>
+        <div class="cell" contenteditable="" data-col="${colIndex}"></div>
     `
 }
-
-function toColumn(col) {
+function toColumn(col, index) {
     return `
-        <div class="column">
+        <div class="column" data-type="resizable" data-index="${index}">
             ${col}    
+            <div class="col-resize" data-resize="col"></div>
         </div>
     `
 }
 function createRow(index, content) {
+    const resize = index
+                    ? `<div class="row-resize" data-resize="row"></div>`
+                    : ''
     return `
-        <div class="row">
-            <div class="row-info">${index ? index : ''}</div>
+        <div class="row" data-type="resizable">
+            <div class="row-info">
+                ${index ? index : ''}
+                ${resize}
+            </div>
             <div class="row-data">${content}</div>
         </div>
     `
@@ -38,7 +43,10 @@ export function createTable(rowsCount = 15) {
                     .join('')
     rows.push(createRow(null, cols))
     for (let i = 0; i < rowsCount; i++) {
-        const cells = new Array(colsCount).fill(createCell()).join('')
+        const cells = new Array(colsCount)
+                    .fill('')
+                    .map(createCell)
+                    .join('')
         rows.push(createRow(i + 1, cells))
     }
     return rows.join('')
